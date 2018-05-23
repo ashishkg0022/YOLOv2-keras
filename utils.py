b78e70c85150dd5d246a5484fab119d63e87bf5b
Ashish Kumar Gaurav <ashishkg0022@gmail.com>
2018-05-24 03:30:57 +0530

    Added Yolo-2

---
+++ utils.py
@@ -0,0 +1,142 @@
+import colorsys
+import imghdr
+import os
+import os
+import random
+import sys
+import numpy as np
+import tensorflow as tf
+import keras.backend as K
+
+import numpy as np
+from PIL import Image, ImageDraw, ImageFont
+
+def scale_boxes(boxes, image_shape ):
+    height = image_shape[0]
+    width = image_shape[1]
+    image_dims = K.stack([height, width, height, width])
+    image_dims = K.reshape(image_dims, [1, 4])
+    boxes = boxes * image_dims
+    return boxes
+
+def yolo_boxes_to_corners(box_xy, box_wh):
+    box_mins = box_xy - (box_wh / 2.)
+    box_maxes = box_xy + (box_wh / 2.)
+
+    return K.concatenate([
+        box_mins[..., 1:2],  # y_min
+        box_mins[..., 0:1],  # x_min
+        box_maxes[..., 1:2],  # y_max
+        box_maxes[..., 0:1]  # x_max
+    ])
+
+
+
+def yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold = .6):
+    box_scores = box_confidence * box_class_probs
+
+    box_classes = K.argmax(box_scores, axis=-1)
+    box_class_scores = K.max(box_scores, axis=-1)
+
+    filtering_mask = box_class_scores >= threshold
+
+    scores = tf.boolean_mask(box_class_scores, filtering_mask)
+    boxes = tf.boolean_mask(boxes, filtering_mask)
+    classes = tf.boolean_mask(box_classes, filtering_mask)
+
+    
+    return scores, boxes, classes
+
+
+
+
+
+def yolo_head(feats, anchors, num_classes):
+    """Convert final layer features to bounding box parameters.
+
+    Parameters
+    ----------
+    feats : tensor
+        Final convolutional layer features.
+    anchors : array-like
+        Anchor box widths and heights.
+    num_classes : int
+        Number of target classes.
+
+    Returns
+    -------
+    box_xy : tensor
+        x, y box predictions adjusted by spatial location in conv layer.
+    box_wh : tensor
+        w, h box predictions adjusted by anchors and conv spatial resolution.
+    box_conf : tensor
+        Probability estimate for whether each box contains any object.
+    box_class_pred : tensor
+        Probability distribution estimate for each box over class labels.
+    """
+    num_anchors = len(anchors)
+    # Reshape to batch, height, width, num_anchors, box_params.
+    anchors_tensor = K.reshape(K.variable(anchors), [1, 1, 1, num_anchors, 2])
+    # Dynamic implementation of conv dims for fully convolutional model.
+    conv_dims = K.shape(feats)[1:3]  # assuming channels last
+    # In YOLO the height index is the inner most iteration.
+    conv_height_index = K.arange(0, stop=conv_dims[0])
+    conv_width_index = K.arange(0, stop=conv_dims[1])
+    conv_height_index = K.tile(conv_height_index, [conv_dims[1]])
+
+    conv_width_index = K.tile(K.expand_dims(conv_width_index, 0), [conv_dims[0], 1])
+    conv_width_index = K.flatten(K.transpose(conv_width_index))
+    conv_index = K.transpose(K.stack([conv_height_index, conv_width_index]))
+    conv_index = K.reshape(conv_index, [1, conv_dims[0], conv_dims[1], 1, 2])
+    conv_index = K.cast(conv_index, K.dtype(feats))
+    
+    feats = K.reshape(feats, [-1, conv_dims[0], conv_dims[1], num_anchors, num_classes + 5])
+    conv_dims = K.cast(K.reshape(conv_dims, [1, 1, 1, 1, 2]), K.dtype(feats))
+
+    box_confidence = K.sigmoid(feats[..., 4:5])
+    box_xy = K.sigmoid(feats[..., :2])
+    box_wh = K.exp(feats[..., 2:4])
+    box_class_probs = K.softmax(feats[..., 5:])
+
+    # Adjust preditions to each spatial grid point and anchor size.
+    # Note: YOLO iterates over height index before width index.
+    box_xy = (box_xy + conv_index) / conv_dims
+    box_wh = box_wh * anchors_tensor / conv_dims
+
+    return box_confidence, box_xy, box_wh, box_class_probs
+
+
+
+def draw_boxes(image, out_scores, out_boxes, out_classes, class_names, colors):
+    
+    font = ImageFont.truetype(font='font/Quicksand-BoldItalic.otf',size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
+    thickness = (image.size[0] + image.size[1]) // 300
+
+    for i, c in reversed(list(enumerate(out_classes))):
+        predicted_class = class_names[c]
+        box = out_boxes[i]
+        score = out_scores[i]
+
+        label = '{} {:.2f}'.format(predicted_class, score)
+
+        draw = ImageDraw.Draw(image)
+        label_size = draw.textsize(label, font)
+
+        top, left, bottom, right = box
+        top = max(0, np.floor(top + 0.5).astype('int32'))
+        left = max(0, np.floor(left + 0.5).astype('int32'))
+        bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
+        right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
+        print(label, (left, top), (right, bottom))
+
+        if top - label_size[1] >= 0:
+            text_origin = np.array([left, top - label_size[1]])
+        else:
+            text_origin = np.array([left, top + 1])
+
+        # My kingdom for a good redistributable image drawing library.
+        for i in range(thickness):
+            draw.rectangle([left + i, top + i, right - i, bottom - i], outline=colors[c])
+        draw.rectangle([tuple(text_origin), tuple(text_origin + label_size)], fill=colors[c])
+        draw.text(text_origin, label, fill=(0, 0, 0), font=font)
+        del draw
